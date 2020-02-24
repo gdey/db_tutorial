c4e240e270379b0384b530a8280f279e8f11c3a5
Gautam Dey <gautam.dey77@gmail.com>
2020-02-23 17:38:44 -0800

    part 2

--- main.go
+++ main.go
@@ -3,35 +3,103 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 )
+
+type MetaCommand uint
+
+const (
+	MetaCommandSuccess MetaCommand = iota
+	MetaCommandExit
+	MetaCommandUnrecognizedCommand
+)
+
+type PrepareResult uint
+
+const (
+	PrepareSuccess PrepareResult = iota
+	PrepareUnrecognizedStatement
+)
+
+type StatementType uint
+
+const (
+	StatementInsert StatementType = iota
+	StatementSelect
+)
+
+type Statement struct {
+	Type StatementType
+}
 
 func printPrompt() {
 	fmt.Printf("db > ")
 }
 
-func readInput(r io.Reader) (string, error) {
-	var val string
-	count, err := fmt.Fscanln(r, &val)
-	if count > 0 {
-		// remove the last newline
-		val = val[:len(val)-2]
+func doMetaCommand(input string) MetaCommand {
+	switch input {
+	case ".exit":
+		return MetaCommandExit
+	default:
+		return MetaCommandUnrecognizedCommand
 	}
-	return val, err
+}
+
+func prepareStatement(input string) (*Statement, PrepareResult) {
+	switch {
+	case strings.HasPrefix(input, "insert"):
+		return &Statement{Type: StatementInsert}, PrepareSuccess
+	case strings.HasPrefix(input, "select"):
+		return &Statement{Type: StatementSelect}, PrepareSuccess
+	default:
+		return nil, PrepareUnrecognizedStatement
+	}
+}
+
+func executeStatement(statement *Statement) {
+	if statement == nil {
+		return
+	}
+	switch statement.Type {
+	case StatementInsert:
+		fmt.Printf("This is where we would do an insert.\n")
+	case StatementSelect:
+		fmt.Printf("This is where we would do an select.\n")
+	}
 }
 
 func main() {
-	printPrompt()
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
+	for {
+		printPrompt()
+		scanner.Scan()
 
-		switch input := scanner.Text(); input {
-		case ".exit":
-			os.Exit(0)
-		default:
-			fmt.Printf("Unrecognized command '%s'.\n", input)
+		input := scanner.Text()
+		if input == "" {
+			continue
 		}
+
+		if input[0] == '.' {
+			switch doMetaCommand(input) {
+			case MetaCommandExit:
+				os.Exit(0)
+			case MetaCommandUnrecognizedCommand:
+				fmt.Printf("Unrecognized command '%s'.\n", input)
+			case MetaCommandSuccess:
+			}
+			continue
+		}
+
+		statement, result := prepareStatement(input)
+		if result == PrepareUnrecognizedStatement {
+			fmt.Printf("Unrecognized keyword at start of '%s'.\n", input)
+			continue
+		}
+
+		executeStatement(statement)
+		fmt.Printf("Executed.\n")
+
 	}
 
 	if err := scanner.Err(); err != nil {
